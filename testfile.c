7fce72409c452e57362f8827652e5c15f3870e0e
root <ilyesmazouzz@gmail.com>
2023-04-26 04:18:57 -0700

    checking the first task

---
+++ testfile.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+/**
+*main - printing a message
+*Return: text message to the terminal
+*/
+
+int main(void)
+{
+	char c[] = "first shell task";
+
+	printf("this is my %s\n", c);
+	return (0);
+}
