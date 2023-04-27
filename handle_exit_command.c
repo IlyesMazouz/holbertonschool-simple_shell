4a4735c0665cc4b9348d55143543c1d4c800afc7
root <ilyesmazouzz@gmail.com>
2023-04-27 14:00:03 -0700

    exit command

--- handle_exit_command.c
+++ handle_exit_command.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#include "limits.h"
 
 /**
 *handle_exit_command - a function that handles the exit command
@@ -9,23 +8,13 @@
 
 void handle_exit_command(char **cmd, char *line)
 {
+
 	if (strcmp(cmd[0], "exit") == 0)
 	{
 	free(line);
 	if (cmd[1])
-	{
-
-	char *endptr;
-	int status = strtol(cmd[1], &endptr, 10);
-
-	if (*endptr != '\0' || status < 0 || status > INT_MAX)
-	{
-	fprintf(stderr, "Invalid exit status\n");
-	exit(EXIT_FAILURE);
-	}
-	exit(status);
-	}
+	exit(atoi(cmd[1]));
 	free_array(cmd);
-	exit(EXIT_SUCCESS);
+	exit(0);
 	}
 }
